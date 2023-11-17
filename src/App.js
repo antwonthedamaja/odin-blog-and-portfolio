da6ac82b8e564e87b129b75b56086a6e6e9f2b9a
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-11-17 10:26:24 -0500

    Add routing to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import Portfolio from './Portfolio';
+import Blog from './Blog';
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/portfolio' element={<Portfolio />}/>
+                <Route path='/blog/*' element={<Blog />}/>
                 <Route index element={<Navigate to="/portfolio" />} />
             </Routes>
         </BrowserRouter>
--- src/Blog.js
+++ src/Blog.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './style/Blog.css';
+import BlogHeader from './components/blog/BlogHeader';
+import BlogIndex from './components/blog/BlogIndex';
+import { Routes, Route } from 'react-router-dom';
 
 export default function Blog() {
     return (
         <>
-        
+            <BlogHeader />
+            <Routes>
+                <Route path='/blog/index' index element={<BlogIndex />} />
+            </Routes>
         </>
     );
 }
