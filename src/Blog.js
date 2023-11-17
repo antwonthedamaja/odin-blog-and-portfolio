0bdc3ae238fe4e5348da354f803d0880731a4597
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-11-17 14:07:52 -0500

    Code fetching of data from API index

--- src/Blog.js
+++ src/Blog.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './style/Blog.css';
 import BlogHeader from './components/blog/BlogHeader';
 import BlogIndex from './components/blog/BlogIndex';
-import { Routes, Route } from 'react-router-dom';
+import { Navigate, Routes, Route } from 'react-router-dom';
 
 export default function Blog() {
     return (
         <>
             <BlogHeader />
             <Routes>
-                <Route path='/blog/index' index element={<BlogIndex />} />
+                <Route path='/index' element={<BlogIndex />} />
+                <Route path='/' element={<Navigate to='/blog/index' />} />
             </Routes>
         </>
     );
