49d0de0e8e1ae95ba42687d22a0b2ea4d1bff48b
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-11-23 13:15:06 -0500

    Added JSON content type to header

--- src/components/blog/CreatePost.js
+++ src/components/blog/CreatePost.js
@@ -1,21 +1,30 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function CreatePost() {
     const [title, setTitle] = useState('');
     const [blogBody, setBlogBody] = useState('');
     const [published, setPublished] = useState(false);
     const [errors, setErrors] = useState([]);
-    
+    const navigate = useNavigate();
+
+
     async function handleSubmit() {
         try {
             const response = await fetch('http://localhost:3000/api/blog', {
                 mode: 'cors',
                 method: 'post',
                 credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
                 body: JSON.stringify({ title, blogBody, published })
             });
             const parsedResponse = await response.json();
             setErrors(parsedResponse.errors);
+            if (response.status === 200) {
+                navigate('..');
+            }
         } catch (err) {
             console.log(err);
         }
@@ -27,7 +36,7 @@ export default function CreatePost() {
         <label htmlFor='body'>Body:</label>
         <textarea name='body' placeholder='Blog body' onChange={e => setBlogBody(e.target.value)} />
         <label htmlFor='published'>Published?</label>
-        <input type='checkbox' name='published' onClick={() => setPublished(!check)} />
+        <input type='checkbox' name='published' onClick={() => setPublished(!published)} />
         <button type='button' onClick={handleSubmit}>Submit blog post</button>
         {errors.map(error => {
             return <div key={error.msg} className='error-text'>{error.msg}</div>;
