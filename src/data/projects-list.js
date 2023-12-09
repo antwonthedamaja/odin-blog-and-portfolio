b68ca543e02f266504613ffc2d3e13f16ed2d293
tony issa <52683702+tonyissa@users.noreply.github.com>
2023-12-09 14:56:27 -0500

    Update projects-list.js

--- src/data/projects-list.js
+++ src/data/projects-list.js
@@ -10,47 +10,47 @@ const data = [
     {
         name: 'Social media clone',
         liveLink: 'https://odin-ig-page.web.app/',
-        codeLink: 'https://github.com/antwonthedamaja/odin-ig-page',
+        codeLink: 'https://github.com/tonyissa/odin-ig-page',
         desc: 'Instagram-inspired social media clone for TOP final Javascript project.',
         image: igpageL,
         key: v4()
     },
     {
         name: 'Sign-up form',
-        liveLink: 'https://htmlpreview.github.io/?https://github.com/antwonthedamaja/sign-up-form/blob/main/index.html',
-        codeLink: 'https://github.com/antwonthedamaja/odin-sign-up-form',
+        liveLink: 'https://htmlpreview.github.io/?https://github.com/tonyissa/sign-up-form/blob/main/index.html',
+        codeLink: 'https://github.com/tonyissa/odin-sign-up-form',
         desc: 'Mock sign-up form showcasing HTML, CSS, and form validation.',
         image: signupL,
         key: v4()
     },
     {
         name: 'Library',
-        liveLink: 'https://htmlpreview.github.io/?https://github.com/antwonthedamaja/library/blob/main/index.html',
-        codeLink: 'https://github.com/antwonthedamaja/odin-library',
+        liveLink: 'https://htmlpreview.github.io/?https://github.com/tonyissa/library/blob/main/index.html',
+        codeLink: 'https://github.com/tonyissa/odin-library',
         desc: 'Library card app to track a book collection.',
         image: libraryL,
         key: v4()
     },
     {
         name: 'Etch-a-sketch',
-        liveLink: 'https://htmlpreview.github.io/?https://github.com/antwonthedamaja/Etch-a-Sketch/blob/main/index.html',
-        codeLink: 'https://github.com/antwonthedamaja/odin-Etch-a-Sketch',
+        liveLink: 'https://htmlpreview.github.io/?https://github.com/tonyissa/Etch-a-Sketch/blob/main/index.html',
+        codeLink: 'https://github.com/tonyissa/odin-Etch-a-Sketch',
         desc: 'Etch-a-sketch HTML app.',
         image: etchasketchL,
         key: v4()
     },
     {
         name: 'Battleships',
-        liveLink: 'https://htmlpreview.github.io/?https://github.com/antwonthedamaja/odin-battleships/blob/main/dist/index.html',
-        codeLink: 'https://github.com/antwonthedamaja/odin-battleships',
+        liveLink: 'https://htmlpreview.github.io/?https://github.com/tonyissa/odin-battleships/blob/main/dist/index.html',
+        codeLink: 'https://github.com/tonyissa/odin-battleships',
         desc: 'Battleships app against a computer player utilizing principles of OOP (object-oriented programming).',
         image: battleshipsL,
         key: v4()
     },
     {
         name: 'Meme memory',
-        liveLink: 'https://antwonthedamaja.github.io/odin-memory-cards/',
-        codeLink: 'https://github.com/antwonthedamaja/odin-memory-cards',
+        liveLink: 'https://tonyissa.github.io/odin-memory-cards/',
+        codeLink: 'https://github.com/tonyissa/odin-memory-cards',
         desc: 'Meme-themed memory card game.',
         image: memememoryL,
         key: v4()
